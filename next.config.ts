1126be92582837bfb80950109e9bb476bdbd636f
Shaun Nielsen <shaunson26@gmail.com>
2026-03-31 09:59:14 +1100

    Update readme, turbopack and db

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+    // For general file tracing (affects "next build")
+  outputFileTracingRoot: path.join(__dirname, '../../'), // Adjust the path as necessary for your structure
+
+  // Specifically for Turbopack (affects "next dev --turbo" or "next build --turbo")
+  turbopack: {
+    root: path.join(__dirname, '../../'), // Adjust the path as necessary
+  },
+  
   reactCompiler: true,
 };
 
